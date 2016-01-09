6b03cf0599b7d39eddc422eb463fe3d6a6b6997f
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-01-09 17:39:37 +0100

    Create Categories model, add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.delete_all
+Product.create(id: 1, name: "Product1", description: "See more snippets like this online store item at Bootsnipp - http://bootsnipp.com.")
+Product.create(id: 2, name: "Product2", description: "See more snippets like this online store item at Bootsnipp - http://bootsnipp.com.")
+Product.create(id: 3, name: "Product3", description: "See more snippets like this online store item at Bootsnipp - http://bootsnipp.com.")
+puts "PRODUCTS CREATED"
+
+Category.create(name: "Category1")
+Category.create(name: "Category2")
+Category.create(name: "Category3")
+Category.create(name: "Category4")
+Category.create(name: "Category5")
+puts "CATEGORIES CREATED"
