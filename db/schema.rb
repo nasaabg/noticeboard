3d399f4a157ae1db845399019d381f4fc751c832
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-01-10 14:32:19 +0100

    Relation between Product and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110122406) do
+ActiveRecord::Schema.define(version: 20160110132317) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20160110122406) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.decimal  "price"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,27 @@
 Product.delete_all
 Category.delete_all
 
-Product.create(id: 1, name: "Apple Iphone", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(id: 2, name: "Apple Macbook", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(id: 3, name: "Harry Potter", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-puts "PRODUCTS CREATED"
-
-Category.create(name: "Category1")
-Category.create(name: "Category2")
-Category.create(name: "Category3")
-Category.create(name: "Category4")
-Category.create(name: "Category5")
+book = Category.create(name: "Books")
+car = Category.create(name: "Cars")
+ele = Category.create(name: "Electronics")
+fasion = Category.create(name: "Fasion")
+toys = Category.create(name: "Toys & Hobbies")
+sport = Category.create(name: "Sport")
+art = Category.create(name: "Collectibles & Art")
+ent = Category.create(name: "Entertainment")
+home = Category.create(name: "Home & Garden")
 puts "CATEGORIES CREATED"
+
+Product.create(category: ele,name: "Apple Iphone", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: ele,name: "Apple Macbook", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: book,name: "Harry Potter", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: fasion,name: "T-Shirt", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: toys,name: "Lego Army", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: sport,name: "Nike Vapor", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: art,name: "Kossak", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: ent,name: "Justin Bieber", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: home,name: "Modern Lamp", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: car,name: "Porshe", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: car,name: "Volvo", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: car,name: "Skoda", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+puts "PRODUCTS CREATED"
