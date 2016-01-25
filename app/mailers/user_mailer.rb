93ce0fcb4a09c1a6f03fe7c6e94e4dcec8a0c429
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-01-25 14:20:08 +0100

    Add specs for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
  
-  def question_email(owner, questioner, question, product)
-    @questioner = questioner
+  def question_email(owner_email, questioner_email, question, product)
+    @questioner_email = questioner_email
     @question  = question
     @product = product
-    mail(to: owner, subject: 'Question from BOARD')
+    mail(to: owner_email, subject: 'Question from BOARD')
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,34 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "question_email" do
+    let(:owner) { create(:user) }
+    let(:questioner) { create(:user) }
+    let(:question) { "My question" }
+    let(:product) { create(:product) }
+    let(:email) { UserMailer.question_email(owner.email, questioner.email, question, product ) }
+
+    it 'renders the subject' do
+      expect(email.subject).to eql('Question from BOARD')
+    end
+
+    it 'renders the receiver email' do
+      expect(email.to).to eql([owner.email])
+    end
+
+    it 'renders the sender email' do
+      expect(email.from).to eql(['notifications@example.com'])
+    end
+
+    it 'assigns @question' do
+      expect(email.body.encoded).to match(question)
+    end
+
+    it 'assigns @product' do
+      expect(email.body.encoded).to match(product.name)
+    end
+    it 'assigns @questioner' do
+      expect(email.body.encoded).to match(questioner.email)
+    end
+  end
 end
