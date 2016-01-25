681ab7a36db3e750ee38141b7cd5f3510713b780
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-01-25 14:31:38 +0100

    Add user specs for points

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 describe User do
-    let(:user) { User.create!(email: "tester@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today) }
-    let(:admin) { User.create!(email: "tester1@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, admin: true) }
-    let(:banned) { User.create!(email: "ban@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, baned: true) }
-    let(:moderator) { User.create!(email: "moderator@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, moderator: true) }
+  let(:user) { User.create!(email: "tester@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today) }
+  let(:admin) { User.create!(email: "tester1@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, admin: true) }
+  let(:banned) { User.create!(email: "ban@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, baned: true) }
+  let(:moderator) { User.create!(email: "moderator@tets.pl", password: "123456789", password_confirmation: "123456789", confirmed_at: Date.today, moderator: true) }
+  
   it "has correct email" do
     expect(user.email).to eq("tester@tets.pl")
   end
@@ -31,5 +32,27 @@ describe User do
 
   it "has correct admin status" do
     expect(user.moderator).to eq(false)
+  end
+
+  describe "#received_points" do
+    let(:rewarder) { create(:user) }
+
+    it "has some received points" do
+      Point.create(receiver: user, rewarder: rewarder)
+      user.save
+      expect(user.received_points.size).to eq(1)     
+    end
+  end
+
+  describe "#given_points" do
+    let(:receiver1) { create(:user) }
+    let(:receiver2) { create(:user) }
+
+    it "has some received points" do
+      Point.create(receiver: receiver1, rewarder: user)
+      Point.create(receiver: receiver2, rewarder: user)
+      user.save
+      expect(user.given_points.size).to eq(2)     
+    end
   end
 end
